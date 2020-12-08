ac9eef8c28ca3bfcfc334d4608c902a87fa37401
sojinj1 <sojinj1@naver.com>
2020-12-08 17:14:51 +0900

    ..

--- TeamProject/app.py
+++ TeamProject/app.py
@@ -176,6 +176,6 @@ api.add_resource(movieInfo, '/moviesearch/movieinfo')
 api.add_resource(cineInfo, '/moviesearch/cineinfo')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port = "5000")
 
 
